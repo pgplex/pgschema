813a9a1c657f157a340030cca28ad27fe24701c2
tianzhou <t@bytebase.com>
2026-08-05 23:21:02 -0700

    add missing plan.sql, plan.json, plan.txt for issue #530 test case

---
+++ testdata/diff/dependency/issue_530_function_table_function_chain/plan.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION random_id()
+RETURNS bigint
+LANGUAGE sql
+VOLATILE
+AS $$
+    SELECT CAST(1000000000 + floor(random() * 9000000000) AS bigint);
+$$;
+
+CREATE TABLE IF NOT EXISTS x (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    public_id text DEFAULT random_id() NOT NULL,
+    name text NOT NULL,
+    flag boolean DEFAULT false NOT NULL,
+    CONSTRAINT x_pkey PRIMARY KEY (id),
+    CONSTRAINT x_name_key UNIQUE (name),
+    CONSTRAINT x_public_id_key UNIQUE (public_id)
+);
+
+CREATE OR REPLACE FUNCTION x_is_flagged(
+    id bigint
+)
+RETURNS boolean
+LANGUAGE sql
+STABLE
+AS $$
+    SELECT x.flag FROM x WHERE x.id = id;
+$$;
